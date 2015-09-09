b4bc575e958e7813f1fd6bc7013d0a1b927e1fc9
Masaki Waga <masakiwaga@gmail.com>
2015-09-09 13:03:39 +0900

    managing way seems work (but too ugly)

--- src/main/scala/cache.scala
+++ src/main/scala/cache.scala
@@ -13,11 +13,16 @@ import Chisel._
 
  Arguments
 =================
+ * wayBits : bit size of lines
  * lineBits : bit size of lines
 
  */
-class Cache (numOfWay : Int,lineBits : Int,wtime : Int) extends Module {
+class Cache (wayBits : Int,lineBits : Int,wtime : Int) extends Module {
   val io = new Bundle {
+    val pohe = UInt (OUTPUT);
+    val hyoe = UInt (OUTPUT);
+    val foo = UInt (OUTPUT);
+    val bar = UInt (OUTPUT);
     val cmdin = Decoupled (new Bundle {
       val we = Bool (INPUT);
       val addr = UInt (width = 21);
@@ -26,12 +31,12 @@ class Cache (numOfWay : Int,lineBits : Int,wtime : Int) extends Module {
       val we = Bool (INPUT);
       val addr = UInt (width = 22);
     });
-    val wdataFromCore = Valid(UInt(width = 16)).flip;
-    val rdataFromCore = Valid(UInt(width = 16));
-    val wdataToDRAM = Valid(UInt(width = 32));
-    val rdataToDRAM = Valid(UInt(width = 32)).flip;
+    val wdataFromCore = Valid(UInt(width = 32)).flip;
+    val rdataFromCore = Valid(UInt(width = 32));
+    val wdataToDRAM = Valid(UInt(width = 16));
+    val rdataToDRAM = Valid(UInt(width = 16)).flip;
   }
-  val time = UInt(wtime, log2Up(wtime) + 1)
+  val time = UInt(wtime-1, log2Down(wtime-1) + 1)
   val wordsPerLine = 4;
 
   // addr width
@@ -42,72 +47,107 @@ class Cache (numOfWay : Int,lineBits : Int,wtime : Int) extends Module {
   // splited addr 
   val tag        = Reg(UInt (width = tagWidth))
   val index      = Reg(UInt (width = indexWidth))
-  val posInWords = Reg(UInt (width = wordsWidth))
+  val posInLine = Reg(UInt (width = wordsWidth))
 
   val numOfLine = 1 << lineBits
+  val numOfWay  = 1 << wayBits
 
   val cacheSize = numOfLine * numOfWay;
 
-  val cache = Mem(UInt(width = 32), cacheSize);
+  val cache = Mem(Vec(UInt(width = 32), wordsPerLine), cacheSize);
   val tagArray = Mem(UInt (width = tagWidth), cacheSize);
+  val nextWay = Mem(UInt(width = log2Up (numOfWay) + 1), numOfLine);
 
   val busy :: ready :: cacheStates = Enum (UInt(),2)
   val state   = Reg (init = ready)
   val count   = Reg (init = time)
-  val wordCnt = Reg (init = UInt(0))
-  val addr    = Reg (init = UInt(0));
+  val wordCnt = Reg (init = UInt(0,3))
+  val addr    = Reg (init = UInt(0,22));
   val we      = Reg (init = Bool(false))
 
-  def isHit : Bool = {
-    for (i <- 0 until wordsPerLine - 1) {
-      if (tag == tagArray (index << wordsWidth + i)) {
-        return Bool(true)
-      }
-    }
-    return Bool(false)
-  }
+  val wdata_from_core = Reg (init = UInt (0,32));
+  val dram_buff = Reg (init = Decoupled(UInt(width = 16)));
 
-  io.cmdout.valid := Bool(false);
+  val core_buff = Reg (init = Decoupled(UInt(width = 32)));
+
+  io.cmdout.valid := (state === busy);
   io.cmdout.bits.addr := addr;
   io.cmdout.bits.we := we;
-  io.rdataFromCore.bits := UInt (0);
-  io.rdataFromCore.valid := Bool(false);
-  io.wdataToDRAM.bits := UInt (0);
-  io.wdataToDRAM.valid := Bool(false);
+  io.rdataFromCore := core_buff;
+  io.wdataToDRAM := dram_buff;
+
+  val pohe = Reg (init = UInt(0));
+  val hyoe = Reg (init = UInt(0));
+  val foo = Reg (init = UInt(0));
+  val bar = Reg (init = UInt(0));
+
+
+  io.pohe := pohe;
+  io.hyoe := hyoe;
+  io.foo := foo;
+  io.bar := bar;
 
   switch (state) {
     is (ready) {
       when (io.cmdin.valid) {
-        when (isHit) {
+        val varIndex = io.cmdin.bits.addr.apply(indexWidth + wordsWidth - 1,wordsWidth);
+        val varTag = io.cmdin.bits.addr.apply(20,indexWidth + wordsWidth);
+        val nowWay = Mux(varTag === tagArray((varIndex << wayBits) + UInt(0)),UInt (0),
+          Mux (varTag === tagArray((varIndex << wayBits) + UInt(1)),UInt (1),UInt(2)))
+        // for (i <- 0 to numOfWay - 1) {
+        //     when (varTag === tagArray ((varIndex << wayBits) + UInt(i))) {
+        //       nowWay = i
+        //     }
+        // }
+        // when (varTag === tagArray((varIndex << wayBits) + UInt(0))) {
+        //   nowWay = UInt(0);
+        // } .elsewhen (varTag === tagArray((varIndex << wayBits) + UInt(1))) {
+        //     nowWay = UInt(1);
+        // } .otherwise {
+        //     nowWay = UInt(2);
+        // }
+
+        pohe := nowWay;
+        hyoe := varTag;
+        foo  := tagArray((varIndex << wayBits) + UInt(0));
+        bar  := tagArray((varIndex << wayBits) + UInt(1));
+
+        when (nowWay != UInt(numOfWay)) {
+          var tmp_posInLine = io.cmdin.bits.addr.apply (wordsWidth - 1,0)
+          dram_buff.valid := Bool (false)
           when (io.cmdin.bits.we) {
             // write
             when (io.wdataFromCore.valid) {
-              cache ((index << wordsWidth) + posInWords) := io.wdataFromCore.bits
+              cache ((varIndex << wayBits) + nowWay)(tmp_posInLine) := io.wdataFromCore.bits
             }
           } .otherwise {
             // read
-            io.rdataFromCore.bits := cache ((index << wordsWidth) + posInWords)
-            io.rdataFromCore.valid := Bool(true)
+            core_buff.bits := cache ((varIndex << wayBits) + nowWay)(tmp_posInLine)
+            core_buff.valid := Bool(true)
           }
         } .otherwise {
+          core_buff.valid := Bool (false)
           state := busy
           count := time
           wordCnt := UInt(0)
 
           // burst access
-          addr := io.cmdin.bits.addr ## UInt (0)
+          when (io.cmdin.bits.we) {
+            // tag index words
+            var tmp_index = io.cmdin.bits.addr.apply(indexWidth + wordsWidth - 1,wordsWidth)
+            addr := tagArray ((tmp_index << wayBits) + nextWay (tmp_index)) ## io.cmdin.bits.addr.apply(indexWidth + wordsWidth - 1,0) ## UInt (0)
+          }.otherwise {
+            addr := io.cmdin.bits.addr ## UInt (0)
+          }
           we   := io.cmdin.bits.we
 
           tag        := io.cmdin.bits.addr.apply(20,indexWidth + wordsWidth);
           index      := io.cmdin.bits.addr.apply(indexWidth + wordsWidth - 1,wordsWidth);
-          posInWords := io.cmdin.bits.addr.apply (wordsWidth - 1,0)
+          posInLine := io.cmdin.bits.addr.apply (wordsWidth - 1,0)
 
-          io.cmdout.bits.addr := addr
-          io.cmdout.bits.we   := we
-          io.cmdout.valid     := Bool(true)
-          when (we) {
-            io.wdataToDRAM.bits := cache ((index << wordsWidth) ^ wordCnt)
-            io.wdataToDRAM.valid := Bool(true)
+          when (io.cmdin.bits.we) {
+            dram_buff.bits := cache (index << wordsWidth) (0).apply (15,0)
+            dram_buff.valid := Bool(true)
           }
         }
       }
@@ -116,35 +156,40 @@ class Cache (numOfWay : Int,lineBits : Int,wtime : Int) extends Module {
       when (count === UInt (0)) {
         count := time
         when (we === Bool(false) && io.rdataToDRAM.valid) {
-          when (wordCnt.apply (0) === UInt(0)) {
-            cache ((index << wordsWidth) ^ wordCnt).apply (15,0) := io.rdataToDRAM.bits
-          }.otherwise{
-            cache ((index << wordsWidth) ^ wordCnt).apply (31,16) := io.rdataToDRAM.bits
+          when (wordCnt (0) === UInt (0)) {
+            cache ((index << wayBits) + nextWay (index))(wordCnt (wordsWidth,1)).apply(15,0) := io.rdataToDRAM.bits
+          }.otherwise {
+            cache ((index << wayBits) + nextWay (index))(wordCnt (wordsWidth,1)).apply(31,16) := io.rdataToDRAM.bits
           }
         }
 
+        // finish
         when (wordCnt === UInt(7)) {
+          when (we) {
+            cache ((index << wayBits) + nextWay (index))(posInLine) := wdata_from_core
+          }.otherwise {
+            core_buff.bits := cache ((index << wayBits) + nextWay (index))(posInLine);
+            core_buff.valid := Bool(true)
+          }
           state := ready
           wordCnt := UInt(0)
-          when (we) {
-            when (io.wdataFromCore.valid) {
-              cache ((index << wordsWidth) + posInWords) := io.wdataFromCore.bits
-            }
+          tagArray ((index << wayBits) + nextWay (index)) := tag
+          when (nextWay (index) === UInt(numOfWay - 1)) {
+            nextWay (index) := UInt (0)
           }.otherwise {
-            io.rdataFromCore.bits := cache ((index << wordsWidth) + posInWords)
-            io.rdataFromCore.valid := Bool(true)
+            nextWay (index) := nextWay (index) + UInt (1);
           }
         } .otherwise {
           wordCnt := wordCnt + UInt (1);
         }
 
         when (we) {
-          when (wordCnt.apply (0) === UInt(0)) {
-            io.wdataToDRAM.bits := cache ((index << wordsWidth) ^ wordCnt).apply(15,0)
-          } .otherwise {
-            io.wdataToDRAM.bits := cache ((index << wordsWidth) ^ wordCnt).apply(31,16)
+          when (wordCnt (0) === UInt (0)) {
+            dram_buff.bits := cache ((index << wayBits) + nextWay (index))(wordCnt (wordsWidth,1)).apply(15,0)
+          }.otherwise {
+            dram_buff.bits := cache ((index << wayBits) + nextWay (index))(wordCnt (wordsWidth,1)).apply(31,16)
           }
-          io.wdataToDRAM.valid := Bool(true)
+          dram_buff.valid := Bool(true)
         }
       } .otherwise {
         count := count - UInt (1);
@@ -156,13 +201,114 @@ class Cache (numOfWay : Int,lineBits : Int,wtime : Int) extends Module {
 }
 
 object Cache {
+  val wayBits = 1;
+  val lineBits = 5;
+  val wtime = 2;
+
   def main (args : Array[String]) : Unit = {
-    chiselMainTest (args, () => Module (new Cache (16,16,2))) { c =>
+    chiselMainTest (args, () => Module (new Cache (wayBits,lineBits,wtime))) { c =>
       new CacheTests (c)
     }
   }
 
-  class CacheTests (c: Cache) extends Tester (c,isTrace = false) {
-    // Not yet
+  class CacheTests (c: Cache) extends Tester (c) {
+
+    def readTest (addr: Int,value: Int,expectMiss: Boolean) {
+      print ("start of read test\n");
+      while (peek (c.io.cmdin.ready) == 0) {
+        step (1);
+      }
+      poke (c.io.cmdin.bits.we,false);
+      poke (c.io.cmdin.bits.addr,addr);
+      poke (c.io.cmdin.valid,true);
+      step (1);
+      pohe ()
+      if (!expectMiss) {
+        expect (c.io.cmdin.ready,1)
+        expect (c.io.rdataFromCore.valid,1)
+        expect (c.io.rdataFromCore.bits,value)
+        print ("end of read test\n");
+        return
+      }
+      expect (c.io.cmdout.valid,1);
+      expect (c.io.cmdout.bits.we,0);
+      expect (c.io.cmdout.bits.addr,addr);
+      for (i <- 0 to 3) {
+        // TODO: In this test,timing is critical
+        // low bit
+        step (wtime)
+        poke (c.io.rdataToDRAM.bits,value & ((1 << 16) - 1));
+        poke (c.io.rdataToDRAM.valid,true);
+
+        // high bit
+        step (wtime);
+        poke (c.io.rdataToDRAM.valid,true);
+        poke (c.io.rdataToDRAM.bits,value >> 16);
+      }
+
+      expect (c.io.rdataFromCore.valid,1)
+      expect (c.io.rdataFromCore.bits,value)
+      print ("end of read test\n");
+    }
+
+    def writeTest (addr: Int,value: Int,expectMiss: Boolean) {
+      print ("start of write test\n");
+      while (peek (c.io.cmdin.ready) == 0) {
+        step (1);
+      }
+      poke (c.io.cmdin.bits.we,true);
+      poke (c.io.cmdin.bits.addr,addr);
+      poke (c.io.cmdin.valid,true);
+      poke (c.io.wdataFromCore.valid,true)
+      poke (c.io.wdataFromCore.bits,value)
+
+      step (1);
+      pohe ()
+      if (!expectMiss) {
+        expect (c.io.cmdin.ready,1)
+        print ("end of write test\n");
+        return
+      }
+      expect (c.io.cmdout.valid,1);
+      expect (c.io.cmdout.bits.we,1);
+      printf ("cmdout addr:%d\n",peek (c.io.cmdout.bits.addr));
+
+      for (i <- 0 to 3) {
+        // low bit
+        printf ("wdata to DRAM:%d\n",peek(c.io.wdataToDRAM.bits));
+
+        expect (c.io.wdataToDRAM.valid,1);
+        step (wtime)
+
+        // high bit
+        printf ("wdata to DRAM:%d\n",peek(c.io.wdataToDRAM.bits));
+        expect (c.io.wdataToDRAM.valid,1);
+        step (wtime);
+      }
+      print ("end of write test\n");
+    }
+
+    // expect 1024 and 1025 are in the same line
+    writeTest (1024,1234567890,true);
+    readTest  (1024,1234567890,false);
+    writeTest (1025,1234567890,false);
+    readTest  (1025,1234567890,false);
+
+    // expect working as 2 nowWay
+    writeTest (1024,234567890,false);
+    writeTest (2048,123456789,true);
+    readTest  (2048,123456789,false);
+    readTest  (1024,234567890,false);
+    def pohe () {
+      printf ("pohe:%d\n",peek (c.io.pohe));
+      peek (c.io.hyoe);
+      peek (c.io.foo);
+      peek (c.io.bar);
+      if (peek (c.io.pohe) == 0) {
+        print("POHE!\n");
+      } else {
+        print("HYOE\n");
+      }
+    }
   }
 }
